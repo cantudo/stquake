aaf27997a1c2626d0a5b0fb14550722dea874e21
Antonio Cantudo <antoniokantudo@gmail.com>
2021-02-10 03:02:14 +0100

    better point selection

--- stquake/src/components/Map.vue
+++ stquake/src/components/Map.vue
@@ -1,31 +1,35 @@
 <template>
   <div id="map">
-    <MglMap :accessToken="accessToken" :mapStyle.sync="mapStyle" @load="onMapLoaded" :center="coordinates" :zoom="defZoom">
+    <MglMap container="map" :accessToken="accessToken" :mapStyle.sync="mapStyle" @load="onMapLoaded" :center="coordinates" :zoom="defZoom">
       <MglGeojsonLayer
               :sourceId="geoJsonSource.data.id"
               :source="geoJsonSource"
               layerId="earthquakes-viz"
               :layer="geoJsonLayer"
       />
+      <MglGeojsonLayer
+              :sourceId="geoJsonSource.data.id"
+              :source="geoJsonSource"
+              layerId="earthquake-labels"
+              :layer="quakeTextLayer"
+      />
     </MglMap>
 
       <v-app-bar-nav-icon style="position: absolute; top:1vh; left:1vh;" dark @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-navigation-drawer v-model="drawer" absolute bottom temporary dark app ref="quake_bar" width="410">
+      <v-navigation-drawer v-model="drawer" absolute bottom temporary dark app ref="quake_bar" :width="drawer_width">
         
         <v-list dense >
-            <v-list-item  two-line>
+            <v-list-item style="margin-bottom: 5px; margin-top: -2px;">
             <v-list-item-content>
-              <v-row no-gutters>
-                <v-col cols="3">
-                  <!-- <v-row> -->
-                    <a class="text-body-2 white--text" >Mag.</a>
-                  <!-- </v-row> -->
-                  <!-- <v-row>
-                    <a class="text-caption" style="opacity: 0;"> stlarx </a>
-                  </v-row> -->
+              <v-row>
+                <v-col cols="1">
+                  <v-spacer></v-spacer>
+                </v-col>
+                <v-col cols="2">
+                    <a class="text-body-2 white--text" > Mag.</a>
                 </v-col>
                 
-                <v-col cols="7" no-gutters style="margin-top: 5px;">
+                <v-col cols="6" style="margin-top: 5px;">
                   <v-row>
                     <a class="text-body-2 white--text font-weight-bold">Location</a>
                   </v-row>
@@ -34,15 +38,15 @@
                   </v-row>
                 </v-col>
 
-                <v-col cols="2" justify="start">
+                <v-col cols="3" justify="center">
                   <a class="text-caption white--text">Depth.</a>
                 </v-col>
 
               </v-row>
             </v-list-item-content>
-            </v-list-item>
+            </v-list-item >
             <v-divider style="border-width:5px;"></v-divider>
-            <!-- <div  style="height: 100%"> -->
+
             <v-virtual-scroll
                 :bench="benched"
                 :items="timeline_items"
@@ -53,19 +57,16 @@
                 <v-list-item ripple link v-on:click="goToQuake(item.id)" two-line>
                   <v-list-item-content>
                     
-                    <!-- <v-list-item-title> -->
-                      <v-row >
+                      <v-row>
+                        <v-col cols="1">
+                            <v-icon :color="item.color">mdi-circle</v-icon>
+                        </v-col>
 
-                        <v-col cols="3">
-                          <!-- <v-row> -->
+                        <v-col cols="2">
                             <a class="text-body-2 white--text" >{{ item.mag }} {{ item.mag_unit }}</a>
-                          <!-- </v-row> -->
-                          <!-- <v-row>
-                            <a class="text-caption" style="opacity: 0;"> stlarx </a>
-                          </v-row> -->
                         </v-col>
                         
-                        <v-col cols="7" no-gutters style="margin-top: 5px;">
+                        <v-col cols="6"  style="margin-top: 5px; margin-bottom: 5px;">
                           <v-row>
                             <a class="text-body-2 white--text font-weight-bold">{{ item.location }}</a>
                           </v-row>
@@ -74,13 +75,11 @@
                           </v-row>
                         </v-col>
 
-                        <v-col cols="2" justify="start">
+                        <v-col cols="3" justify="end">
                           <a class="text-caption white--text">{{ item.depth }}</a>
                         </v-col>
 
-                      </v-row>
-                    <!-- </v-list-item-title> -->
-                    
+                      </v-row>                    
 
                   </v-list-item-content>
                 </v-list-item>
@@ -112,6 +111,7 @@ export default {
     return {
       isActive : false,
       drawer: false,
+      drawer_width: 450,
       benched: 2,
       quake_bar_height: 0,
       accessToken: "pk.eyJ1IjoiY2FhYW50dSIsImEiOiJja2twbXhlb2UwZWc5Mm5zMTIyejNsc2g5In0.MCaZ1fA3fCJ9_YXEcFUXJQ", // your access token. Needed if you using Mapbox maps
@@ -139,7 +139,7 @@ export default {
               ],
               [
                 'interpolate', ['linear'],
-                ['get', 'mag'],
+                ['to-number', ['get', 'mag']],
                 1, '#fff7ec',
                 1.5, '#fee8c8',
                 2, '#fdd49e',
@@ -159,24 +159,57 @@ export default {
               ['feature-state', 'hover'],
               false
             ],
-            [
-              'interpolate', ['linear'],
-              ['get', 'mag'],
-              1, 8,
-              1.5, 10,
-              2, 12,
-              2.5, 14,
-              3, 16,
-              3.5, 18,
-              4.5, 20,
-              6.5, 22,
-              8.5, 24,
-              10.5, 26
-            ],
+            30,
+            // [
+            //   'interpolate', ['linear'],
+            //   ['to-number', ['get', 'mag']],
+            //   1, 8,
+            //   1.5, 10,
+            //   2, 12,
+            //   2.5, 14,
+            //   3, 16,
+            //   3.5, 18,
+            //   4.5, 20,
+            //   6.5, 22,
+            //   8.5, 24,
+            //   10.5, 26
+            // ],
             5
+          ]},
+      },
+      quakeTextLayer: {
+        'id': 'earthquake-labels',
+        'type': 'symbol',
+        'source': 'earthquakes',
+        'replaceSource': true,
+        // 'minzoom': 5,
+        'layout': {
+          'text-field': [
+            'concat',
+            ['to-string', ['get', 'mag']],
+              'm'
+          ],
+          'text-font': [
+            'Open Sans Bold',
+            'Arial Unicode MS Bold'
+            ],
+          'text-size': 20,
+          'text-allow-overlap': true
+        },
+        // 'text-anchor': 'top',
+        'paint': {
+          'text-color': [
+            'case',
+            ['boolean',
+              ['feature-state', 'hover'],
+              false
+            ],
+            'rgba(0,0,0,1)',
+            'rgba(0,0,0,0)'
           ]
         }
       },
+
       timeline_items: []
     };
   },
@@ -188,27 +221,47 @@ export default {
     this.map = null;
     this.getData().then((res) => {
       for (let i = 0; i < res.dias30.features.length; i++) {
-        res.dias30.features[i].properties.hover = false;
+        // res.dias30.features[i].properties.hover = false;
         res.dias30.features[i].properties.mag = parseFloat(res.dias30.features[i].properties.mag);
-
       }
       for (let i = 0; i < res.dias10.features.length; i++) {
-        res.dias10.features[i].properties.hover = false;
+        // res.dias10.features[i].properties.hover = false;
         res.dias10.features[i].properties.mag = parseFloat(res.dias10.features[i].properties.mag);
-
       }
       for (let i = 0; i < res.dias3.features.length; i++) {
-        res.dias3.features[i].properties.hover = false;
+        // res.dias3.features[i].properties.hover = false;
         res.dias3.features[i].properties.mag = parseFloat(res.dias3.features[i].properties.mag);
       }
       this.geoJsonSource.data.features = res.dias30.features;
-
-
     });
 
   },
-  destroyed () {
-    window.addEventListener("resize", this.handle_navbar_scroller);
+  // destroyed () {
+  //   // window.addEventListener("resize", this.handle_navbar_scroller);
+  // },
+
+  watch: {
+    drawer: function () {
+        let padding;
+        if (this.drawer) {
+          if(this.$refs.quake_bar.$el.classList[4] == "v-navigation-drawer--is-mobile") {
+            padding = {bottom: this.$refs.quake_bar.$el.clientHeight, left:0};
+          } else {
+            padding = {bottom: 0, left:this.drawer_width};
+          }
+        } else {
+          padding = {bottom: 0, left: 0};
+        }
+        (async() => {
+            // waiting for geoJson
+            while( this.map == null ) 
+                await new Promise(resolve => setTimeout(resolve, 100));
+            this.map.easeTo({
+              padding: padding,
+              duration: 400 // In ms, CSS transition duration property for the sidebar matches this value
+            });
+        })();
+      },
   },
 
   methods: {
@@ -216,7 +269,7 @@ export default {
         try {
           this.quake_bar_height = this.$refs.quake_bar.$el.clientHeight-10;
         } catch (e) {
-          console.log(e.message);
+          // console.log(e.message);
         }
         return
       },
@@ -247,22 +300,23 @@ export default {
       onMapLoaded(event) {
         // in component
         this.map = event.map;
-
+ 
         let quakeID = null;
         this.map.on('mousemove', 'earthquakes-viz', (e) => {
           this.map.getCanvas().style.cursor = 'pointer';
 
           if (e.features.length > 0) {
-
             if (quakeID) {
               this.map.removeFeatureState({
                 source: "earthquakes",
                 id: quakeID
               });
             }
-
+            // console.log(e.features)
             quakeID = e.features[0].id;
-
+            // console.log(this.map.querySourceFeatures('earthquakes', {
+            //   sourceLayer: 'earthquakes-viz'})
+            // );
             // When the mouse moves over the earthquakes-viz layer, update the
             // feature state for the feature under the mouse
             this.map.setFeatureState({
@@ -292,10 +346,12 @@ export default {
         (async() => {
             // waiting for geoJson
             while(typeof this.geoJsonSource.data.features[0] == 'undefined') 
-                await new Promise(resolve => setTimeout(resolve, 1000));
+                await new Promise(resolve => setTimeout(resolve, 100));
             // geoJson loaded
+
+            let mon_component;
             for (let i = 0; i < this.geoJsonSource.data.features.length; i++) {
-              let mon_component;
+              
               if (this.geoJsonSource.data.features[i].properties.mag > 4) {
                 mon_component = {"x-small": false};
               } else {
@@ -307,20 +363,74 @@ export default {
                                           mag: this.geoJsonSource.data.features[i].properties.mag,
                                           mag_unit: this.geoJsonSource.data.features[i].properties.magtype,
                                           time: this.geoJsonSource.data.features[i].properties.fechalocal,
-                                          depth: (this.geoJsonSource.data.features[i].properties.depth ? this.geoJsonSource.data.features[i].properties.depth.concat(" km") : "")
+                                          depth: (this.geoJsonSource.data.features[i].properties.depth ? this.geoJsonSource.data.features[i].properties.depth.concat(" km") : ""),
+                                          color: this.interpolate_colors(parseFloat(this.geoJsonSource.data.features[i].properties.mag))
                                           });
             }
         })();
- 
+
       },
       
       goToQuake: function (quake_id) {
+        if (this.previous_selected != null) {
+          this.map.removeFeatureState({
+              source: 'earthquakes',
+              id: this.previous_selected
+          });
+        }
+        this.previous_selected = quake_id;
+        this.map.setFeatureState({
+              source: 'earthquakes',
+              id: quake_id
+            }, {
+              hover: true
+        });
+        
         this.map.flyTo({
           center: this.geoJsonSource.data.features[quake_id].geometry.coordinates,
           essential: true, // this animation is considered essential with respect to prefers-reduced-motion,
           zoom: 13
         });
       },
+      interpolate_colors: function (mag) {
+        function interpolate_number(a, b, t) {
+          return (a * (1 - t)) + (b * t);
+        }
+        let colors = {
+          1: '#fff7ec',
+          1.5: '#fee8c8',
+          2: '#fdd49e',
+          2.5: '#fdbb84',
+          3: '#fc8d59',
+          3.5: '#ef6548',
+          4.5: '#d7301f',
+          6.5: '#b30000',
+          8.5: '#7f0000',
+          10.5: '#000000'
+        }
+        let keys = Object.keys(colors);
+        let index = null;
+        for(let i = 0; i < keys.length; i++){
+          if (keys[i] > mag) {
+            index = i;
+            break;
+          }
+        }
+        // console.log(mag)
+        if (index == 0) return "#fff7ec";
+        let from = colors[keys[index - 1]];
+        let to = colors[keys[index]];
+        let percent = (mag-keys[index-1])/(keys[index]-keys[index-1]);
+        let red = interpolate_number(parseInt('0x' + (from.slice(1,3))), parseInt('0x' + (to.slice(1,3))), percent);
+        let blue = interpolate_number(parseInt('0x' + (from.slice(3,5))), parseInt('0x' + (to.slice(3,5))), percent);
+        let green = interpolate_number(parseInt('0x' + (from.slice(5,7))), parseInt('0x' + (to.slice(5,7))), percent);
+        return  '#' + Math.floor(red).toString(16) + Math.floor(blue).toString(16) + Math.floor(green).toString(16);
+
+
+        // console.log(colors);
+        // console.log(mag);
+        // return null;
+      }
     },
 
     mounted () {
@@ -329,7 +439,7 @@ export default {
       } catch (e) {
         console.log(e.message);
       }
-    }
+    },
 };
 
 
